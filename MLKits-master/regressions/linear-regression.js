9385dec450ab13834d65f3bc54eb0e76d835c46a
ivndz <ivan@endbracket.com>
2019-10-31 17:56:41 -0700

    s6v76

--- MLKits-master/regressions/linear-regression.js
+++ MLKits-master/regressions/linear-regression.js
@@ -4,9 +4,23 @@ class LinearRegression {
     constructor(features, labels, options){
         this.features = features
         this.labels = labels
-
-        this.options = Object.assign({ learningRate: 0.1 }, options)
+        //assigns a default value to a new object.
+        //new LinearRegression(features, labels, { learningRate: 0.025 })
+        // this would assign iterations 1000 but overwrite the learningRate.
+        //
+        this.options = Object.assign({ 
+            learningRate: 0.1, 
+            iterations: 1000 
+        }, options)
         
+    }
+
+    train(){
+        
+        for(let i = 0; i < this.options.iterations; i++){
+            this.gradientDescent()
+
+        }
     }
 }
 
