ff4a50772618ca46cf64937994b6985ee80c67c7
ivndz <ivan@endbracket.com>
2019-11-02 10:36:39 -0700

    s7v86

--- MLKits-master/regressions/linear-regression.js
+++ MLKits-master/regressions/linear-regression.js
@@ -21,27 +21,42 @@ class LinearRegression {
             iterations: 1000 
         }, options)
 
-        this.m = 0
-        this.b = 0
+        this.weights = tf.zeros([2,1])
+        
+        // this.m = 0
+        // this.b = 0
         
     }
 
     gradientDescent(){
-        const currentGuessesForMGP = this.features.map(row => {
-            return this.m * row[0] + this.b
-        })
+        
+        const currentGuesses = this.features.matMul(this.weights)
+        const differences = currentGuesses.sub(this.labels)
 
-        const bSlope = _.sum(currentGuessesForMGP.map((guess, i) => {
-            return guess - this.labels[i][0]
-        })) * 2 / this.features.length
-
-        const mSlope = _.sum(currentGuessesForMGP.map((guess, i) => {
-            return -1 * this.features[i][0] * (this.labels[i][0] - guess)
-        })) * 2 / this.features.length
-
-        this.m = this.m - mSlope * this.options.learningRate
-        this.b = this.b - bSlope * this.options.learningRate
+        const slopes = this.features
+            .transpose()
+            .matMul(differences)
+            .div(this.features.shape[0])
     }
+
+
+    // old array based gradient descent
+    // gradientDescent(){
+    //     const currentGuessesForMGP = this.features.map(row => {
+    //         return this.m * row[0] + this.b
+    //     })
+
+    //     const bSlope = _.sum(currentGuessesForMGP.map((guess, i) => {
+    //         return guess - this.labels[i][0]
+    //     })) * 2 / this.features.length
+
+    //     const mSlope = _.sum(currentGuessesForMGP.map((guess, i) => {
+    //         return -1 * this.features[i][0] * (this.labels[i][0] - guess)
+    //     })) * 2 / this.features.length
+
+    //     this.m = this.m - mSlope * this.options.learningRate
+    //     this.b = this.b - bSlope * this.options.learningRate
+    // }
 
     train(){
         
